d7eb772de384cae22d4ffdde17f819f695408b7b
santosh <santoorvlss4321@gmail.com>
2022-03-25 18:09:12 +0530

    trying to add different site icon

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,4 @@
-import Login1 from "./login_image1.jpg";
-import Login2 from "./login_image2.webp";
 import Login3 from "./login_image3.webp";
 import GoogleImg from "./googleImg.png";
-export { Login1, Login2, Login3, GoogleImg };
+import DashbordImg from "./DashboardImg.webp";
+export { Login3, GoogleImg, DashbordImg };
